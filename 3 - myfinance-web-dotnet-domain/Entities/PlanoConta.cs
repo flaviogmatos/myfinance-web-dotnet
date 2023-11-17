96bc3a85dbab8dd9ec3435c55f8dc1ef8506d373
flaviogmatos <flaviogmatos@gmail.com>
2023-11-17 10:09:12 -0300

    Evolução

--- 3 - myfinance-web-dotnet-domain/Entities/PlanoConta.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace myfinance_web_dotnet_domain.Entities;
-
-public class PlanoConta
-{
-    public int? Id { get; set; }
-    public string Descricao { get; set; }
-    public string Tipo { get; set; }
-}
