6299455a3c42d7cf25c5c060570777714a673256
Flávio <flaviogmatos@gmail.com>
2023-11-24 20:52:38 -0300

    Evolução

---
+++ 4 - myfinance-web-dotnet-infra/Interfaces/IPlanoContaRepository.cs
@@ -0,0 +1,14 @@
+﻿using myfinance_web_dotnet_domain.Entities;
+using myfinance_web_dotnet_infra.Interfaces.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace myfinance_web_dotnet_infra.Interfaces
+{
+    public interface IPlanoContaRepository : IRepository<PlanoConta>
+    {
+    }
+}
